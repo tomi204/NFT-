6b0cadc7b07ef1b9364e3ea8c770deecb317ec55
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-10-12 11:42:55 -0500

    Terminado-Token ERC721

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,28 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFT is ERC721URIStorage { 
+contract MyNFT is ERC721 {
+    uint256 token_count;
 
-    using Counters for Counters.Counter; 
-    Counters.Counter private _tokenIds;
-    address owner;
+    constructor() ERC721("T+G", "MNFT") {}
 
-    constructor() ERC721("NFT", "ENFT") {
-        owner = payable (msg.sender);
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return "https://ipfs.io/ipfs/QmXMjXEjWBAzfA3UGPEQPub8iZAqD8myK12jKpE4F9EGfe";
     }
 
-    function createToken(string memory tokenURI) public payable returns (uint) {
-        require(owner == msg.sender);
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+    function mintNFT(address to) public
+    {
+        token_count  += 1;
+        _mint(to, token_count);
     }
 }
